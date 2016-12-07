e2fb6ecf2f2524f531e53043d6fda831015d3e4d
Halimat Mercy Oseni <mehrceey04@gmail.com>
2016-12-07 06:35:12 +0100

    initial commit

---
+++ javascript/calendarmonth.js
@@ -0,0 +1,139 @@
+function refresh (){ 
+  document.getElementById("relod").innerHTML = "";
+  selectDate();
+}
+
+function setToday() {
+  var todaydate   = new Date();
+  var day   = todaydate.getDate();
+  var month = todaydate.getMonth();
+  var year  = todaydate.getYear();
+  if (year < 2000) 
+  year = year + 1900; 
+  this.focusDay = day;
+  document.getElementById("monthValue").value = month;
+  document.getElementById("selectElementId").value = year;
+  refresh();
+}
+
+function setPreviousYear() {
+  var year  = document.getElementById("selectElementId").value;
+  var day   = 0;
+  var month = document.getElementById("monthValue").value;
+  year--;
+  document.getElementById("selectElementId").value = year;
+  refresh();
+}
+
+function setPreviousMonth() {
+  var year  = document.getElementById("selectElementId").value;
+  var day   = 0;
+  var month = document.getElementById("monthValue").value;
+  if (month == 0) {
+    month = 11;
+    if (year > 1000) {
+      year--;
+      document.getElementById("selectElementId").value = year;
+      }
+  } else { month--; }
+  document.getElementById("monthValue").value = month;
+refresh();
+}
+
+function setNextMonth() {
+  var year  = document.getElementById("selectElementId").value;
+  var day   = 0;
+  var month = document.getElementById("monthValue").value;
+  if (month == 11) {
+    month = 0;
+    year++;
+    document.getElementById("selectElementId").value = year;
+  } else { month++; }
+  document.getElementById("monthValue").value = month;
+  refresh();
+}
+
+function setNextYear() {
+  var year = document.getElementById("selectElementId").value;
+  var day = 0;
+  var month = document.getElementById("monthValue").value;
+  year++;
+  document.getElementById("selectElementId").value = year;
+  refresh();
+}
+
+function selectDate() {
+  var month =   document.getElementById("monthValue").value;
+  var year = document.getElementById("selectElementId").value ;
+  calendar(month, year);
+}
+
+
+
+function calendar(Month, Year) {
+  // If no parameter is passed use the current date.
+  if(Month == null && Year == null) {
+      date = new Date();
+      day = date.getDay();
+      month = date.getMonth();
+      year = date.getFullYear();
+} else {
+    date = new Date();
+    day = date.getDate();
+    month = Month;
+    year = Year;
+}
+
+  months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
+  daysInAMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  currentDay = new Date();
+  currentDate = currentDay.getDate();
+  currentMonth = currentDay.getMonth();
+  currentYear = currentDay.getFullYear();
+
+  this_month = new Date(year, month, 1);
+  
+
+  // Find out when this month starts and ends.
+  first_week_day = this_month.getDay();
+    if (month == 1 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)) {
+      days_in_this_month = 29;
+    } else {
+      days_in_this_month = daysInAMonth[month];
+  }
+
+  calendar_html = '<table class = "monthtable" style = "background-color:#ffffff; width: 45%; margin: 0 auto 20px auto;">';
+  calendar_html += '<tr style = "border: 2px solid black;"><td colspan=10 style= "color:000000; text-align: center; font-size: 30px;">' + '<strong>' + months[month]  + ' '  + year + '</strong>' + '</td></tr>';
+  calendar_html += '<tr style = "text-align: center;"><td><b>Sun</b></td><td><b>Mon</b></td><td><b>Tue</b></td><td><b>Wed</b></td><td><b>Thur</b></td><td><b>Fri</b></td><td><b>Sat</b></td></tr>';
+  calendar_html += '<tr>';
+
+  // Fill the first week of the month with the appropriate number of blanks.
+  for(week_day = 0; week_day < first_week_day; week_day++) {
+    calendar_html += '<td style = "background-color:#0; color:000000;"> </td>';
+  }
+
+  week_day = first_week_day;
+  for(day_counter = 1; day_counter <= days_in_this_month; day_counter++) {
+    week_day %= 7;
+    if(week_day == 0)
+      calendar_html += '</tr><tr>';
+
+    // Higlight the current day.
+    
+    if(currentDate == day_counter && currentMonth == Month && currentYear == parseInt(Year)){
+
+         console.log('day_counter',day_counter, 'currentDate', currentDate, Month, parseInt(Year));
+      calendar_html += '<td style = "text-align: center; background-color:green;"><b>' + day_counter + '</b></td>';}
+    else{
+      calendar_html += '<td style = "background-color:9999cc; color:000000; text-align: center;"> ' + day_counter + ' </td>';
+    }
+
+    week_day++;
+  }
+
+  calendar_html += '</tr>';
+  calendar_html += '</table>';
+
+  // Display the calendar.
+ document.getElementById("relod").innerHTML += calendar_html;
+}
---
+++ javascript/calendaryear.js
@@ -0,0 +1,130 @@
+function myCreateFunction() {
+  var year =document.getElementById("selectElementId").value;
+  var table = document.createElement("table");
+  var row = table.insertRow(0);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  var cell4 = row.insertCell(3);
+  cell1.innerHTML = calendar(0, year);
+  cell2.innerHTML = calendar(1, year);
+  cell3.innerHTML = calendar(2, year);
+  cell4.innerHTML = calendar(3, year);
+  var row1 = table.insertRow(1);
+  var cell5 = row1.insertCell(0);
+  var cell6 = row1.insertCell(1);
+  var cell7 = row1.insertCell(2);
+  var cell8 = row1.insertCell(3);
+  cell5.innerHTML = calendar(4, year);
+  cell6.innerHTML = calendar(5, year);
+  cell7.innerHTML = calendar(6, year);
+  cell8.innerHTML = calendar(7, year);
+  var row2 = table.insertRow(2);
+  var cell9 = row2.insertCell(0);
+  var cell10 = row2.insertCell(1);
+  var cell11 = row2.insertCell(2);
+  var cell12 = row2.insertCell(3);
+  cell9.innerHTML = calendar(8, year);
+  cell10.innerHTML = calendar(9, year);
+  cell11.innerHTML = calendar(10, year);
+  cell12.innerHTML = calendar(11, year);
+  return table.outerHTML;
+    
+}
+
+function refresh (){ 
+  document.getElementById("monthlyCal").innerHTML = myCreateFunction();
+}
+
+function setToday() {
+  var now   = new Date();
+  var year  = now.getYear();
+  document.getElementById("selectElementId").value = year;
+  refresh();
+}
+
+function setPreviousYear() {
+  var year  = document.getElementById("selectElementId").value;
+  year--;
+  document.getElementById("selectElementId").value = year;
+  refresh();
+}
+
+function setNextYear() {
+  var year = document.getElementById("selectElementId").value;
+  year++;
+  document.getElementById("selectElementId").value = year;
+  refresh();
+}
+
+function selectDate() {
+  var year = document.getElementById("selectElementId").value ;
+  calendar(month, year);
+}
+
+
+function calendar(Month, Year) {
+  // If no parameters are passed use the current date.
+  if(Month == null && Year == null) {
+      date = new Date();
+      day = date.getDate();
+      month = date.getMonth();
+      year = date.getFullYear();
+} else {
+    date = new Date();
+    day = date.getDate();
+    month = Month;
+    year = Year;
+}
+
+  months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
+  daysInAMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  currentDay = new Date();
+  currentDate = currentDay.getDate();
+  currentMonth = currentDay.getMonth();
+  currentYear = currentDay.getFullYear();
+
+
+  this_month = new Date(year, month, 1);
+  
+
+  // Find out when this month starts and ends.
+  first_week_day = this_month.getDay();
+  if (month == 1 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)) {
+    days_in_this_month = 29;
+    } else {
+    days_in_this_month = daysInAMonth[month];
+    }
+  calendar_html = '<table style = "background-color:#ffffff;">';
+  calendar_html += '<tr style = "border: 4px solid black;"><td colspan = "10" style="background-color:#ffffff; color:#000000; text-align: center;">' +'<strong>' + months[month] + ' ' + year + '</strong>' + '</td></tr>';
+  calendar_html += '<tr style = "text-align: center ;"><td>Sun</td><td>Mon</td><td>Tue</td><td>Wed</td><td>Thur</td><td>Fri</td><td>Sat</td></tr>';
+  calendar_html += '<tr>';
+
+  // Fill the first week of the month with the appropriate number of blanks.
+  for(week_day = 0; week_day < first_week_day; week_day++) {
+    calendar_html += '<td style = "background-color:9999cc; color:000000;"> </td>';
+  }
+
+  week_day = first_week_day;
+  for(day_counter = 1; day_counter <= days_in_this_month; day_counter++) {
+    week_day %= 7;
+    if(week_day == 0)
+      calendar_html += '</tr><tr>';
+
+    // Highlight the current day.
+    
+    if(currentDate == day_counter && currentMonth == Month && currentYear == parseInt(Year))
+      calendar_html += '<td><b>' + day_counter + '</b></td>';
+    else
+      calendar_html += '<td> ' + day_counter + ' </td>';
+
+    week_day++;
+  }
+
+  calendar_html += '</tr>';
+  calendar_html += '</table>';
+
+  // Display the calendar.
+ return calendar_html;
+}
+
